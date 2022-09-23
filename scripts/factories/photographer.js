a1e8ca21e77dbd4de1fe2f98b4e86bb05ce54cf9
Mickael <mickael.krauth@epita.fr>
2022-09-24 01:03:58 +0200

    printing corresponding data in photographer page in console

--- scripts/factories/photographer.js
+++ scripts/factories/photographer.js
@@ -1,17 +1,48 @@
+function setAttributes(el, attrs) {
+  for (let key in attrs) {
+    el.setAttribute(key, attrs[key]);
+  }
+}
+
 function photographerFactory(data) {
-    const { name, portrait } = data;
-
-    const picture = `assets/photographers/${portrait}`;
-
-    function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
-        img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
-        h2.textContent = name;
-        article.appendChild(img);
-        article.appendChild(h2);
-        return (article);
+  const { name, portrait, id, city, country, tagline, price } = data;
+  console.log(data);
+  //   const picture = `assets/photographers/${portrait}`;
+  const picture = `assets/Sample Photos/Photographers ID Photos/${portrait}`;
+  function getUserCardDOM() {
+    const article = document.createElement("article");
+    const img = document.createElement("img");
+    const quote = document.createElement("p");
+    const city_country = document.createElement("p");
+    const priceDay = document.createElement("p");
+    const lien = document.createElement("a");
+    setAttributes(city_country, { class: "city_country" });
+    city_country.textContent = `${city}, ${country}`;
+    setAttributes(quote, { class: "quote" });
+    quote.textContent = `${tagline}`;
+    setAttributes(priceDay, { class: "price" });
+    if (name.length > 14) {
+      img.setAttribute("class", "longName");
     }
-    return { name, picture, getUserCardDOM }
+    priceDay.textContent = `${price}€/jour`;
+    // img.setAttribute("src", picture);
+    // img.setAttribute("alt", `Photo de profile de ${name}`);
+    setAttributes(img, { src: picture });
+    img.setAttribute("aria-label", `Lien vers la page de ${name}`);
+    lien.setAttribute("class", "photoLink");
+    const h2 = document.createElement("h2");
+    h2.textContent = name;
+    console.log(`Id is ${id}`);
+    lien.setAttribute("href", `./photographer.html?id=${id}`);
+    lien.appendChild(img);
+    lien.appendChild(h2);
+    // article.appendChild(img);
+    // article.appendChild(h2);
+    article.appendChild(lien);
+    article.appendChild(city_country);
+    article.appendChild(quote);
+    article.appendChild(priceDay);
+    return article;
+  }
+  return { name, picture, id, city, country, tagline, price, getUserCardDOM };
 }
--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -1,1 +1,40 @@
 //Mettre le code JavaScript lié à la page photographer.html
+
+let params = new URL(document.location).searchParams;
+// console.log(params.get("id"));
+let id = parseInt(params.get("id"));
+// const id = window.location.search;
+console.log(id);
+
+//find
+async function getPhotographers() {
+  // Penser à remplacer par les données récupérées dans le json
+  const photographers = await fetch("./data/photographers.json")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Error loading data" + response.status);
+      }
+      return response.json();
+    })
+    .then((json) => {
+      console.log(json["photographers"]);
+      return json["photographers"];
+    });
+  // et bien retourner le tableau photographers seulement une fois
+  return {
+    photographers: [...photographers],
+  };
+}
+
+async function init() {
+  // Récupère les datas des photographes
+  const { photographers } = await getPhotographers();
+  console.log(photographers);
+  for (let key in photographers) {
+    if (photographers[key].id === id) {
+      console.log(photographers[key]);
+    }
+  }
+}
+
+init();
